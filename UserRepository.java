854f21edb5991bd2f8ae59935808638b5943996e
PauloAMatos777 <154490278+PauloAMatos777@users.noreply.github.com>
2024-09-02 15:52:26 -0300

    Create UserRepository.java

---
+++ UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.crudspring.repository;
+
+import com.example.crudspring.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
